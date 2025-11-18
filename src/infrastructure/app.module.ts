43e2bb631f8578c07c1027dfa789ddfaa5cf1de1
Martin de Leon <madeleon+hebgit@hebmex.com>
2025-11-17 23:44:31 -0600

    feat: [TASK-184730] register GetInfoUseCase in app module

--- src/infrastructure/app.module.ts
+++ src/infrastructure/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { HealthController } from './controllers/health.controller';
 import { NestConfigEnvironmentService } from './services/nest-config-environment.service';
-import { GetHealthUseCase } from '../application/use-cases/get-health.use-case';
+import { GetHealthUseCase } from '@/application/use-cases/get-health.use-case';
 import {
   HEALTH_TOKENS,
   INFRASTRUCTURE_TOKENS,
-} from '../application/config/tokens';
+} from '@/application/config/tokens';
+import { GetInfoUseCase } from '@/application/use-cases/get-info.use-case';
 
 @Module({
   imports: [
@@ -24,6 +25,10 @@ import {
     {
       provide: HEALTH_TOKENS.GET_HEALTH_USE_CASE,
       useClass: GetHealthUseCase,
+    },
+    {
+      provide: HEALTH_TOKENS.GET_INFO_USE_CASE,
+      useClass: GetInfoUseCase,
     },
   ],
 })

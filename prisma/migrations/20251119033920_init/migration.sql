c62c12c47a0a1994c246eb955505cd685838601c
Martin de Leon <madeleon+hebgit@hebmex.com>
2025-11-18 22:53:48 -0600

    feat: [TASK-184730] add initial migration for users table and indexes

---
+++ prisma/migrations/20251119033920_init/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_created_at_idx" ON "users"("created_at");
+
+-- CreateIndex
+CREATE INDEX "users_deleted_at_created_at_idx" ON "users"("deleted_at", "created_at");
+
+-- CreateIndex
+CREATE INDEX "users_email_deleted_at_idx" ON "users"("email", "deleted_at");
